b6a52107fb49a2ece7854b6f0b17cb752e98848e
Justyna <zielonydomek@gmail.com>
2020-12-06 16:14:39 +0100

    refactor

--- src/main/java/pl/time/TimeFormat.java
+++ src/main/java/pl/time/TimeFormat.java
@@ -1,5 +1,6 @@
 package pl.time;
 
+
 public class TimeFormat {
     private static final int MAX_TIME = 359999;
 
@@ -11,27 +12,47 @@ public class TimeFormat {
         if (seconds > MAX_TIME) {
             throw new IllegalArgumentException("Seconds should be smaller than 359999");
         }
+        String hoursReadable = calculateHours(seconds);
+        String minutesReadable = calculateMinutes(seconds);
+        String secondsReadable = calculateSeconds(seconds);
 
-        int secondsUnit = 3600;
-        StringBuffer stringBuffer = new StringBuffer();;
-        for(int i = 0; i <3; i++) {
-            int time = seconds/secondsUnit;
-            seconds = seconds % secondsUnit;
-            secondsUnit = secondsUnit/60;
-            if (isZeroNeeded(time)) {
-                stringBuffer.append("0");
-            }
-            stringBuffer.append(time);
-            if (isNotLastElement(i)) {
-                stringBuffer.append(":");
-            }
-
-        }
-        return stringBuffer.toString();
+        return String.join(":", new String[]{hoursReadable, minutesReadable, secondsReadable});
     }
 
-    private static boolean isNotLastElement(int i) {
-        return i < 2;
+    private static String calculateSeconds(int seconds) {
+        StringBuilder stringBuilder = new StringBuilder();
+        seconds = seconds % 3600;
+        seconds = seconds % 60;
+        if(isZeroNeeded(seconds)){
+            stringBuilder
+                    .append("0");
+        }
+        stringBuilder.append(seconds);
+        return stringBuilder.toString();
+    }
+
+    private static String calculateHours(int seconds){
+        StringBuilder stringBuilder = new StringBuilder();
+        int hours = seconds / 3600;
+        if (isZeroNeeded(hours)) {
+            stringBuilder
+                    .append("0");
+        }
+        stringBuilder
+                .append(hours);
+        return stringBuilder.toString();
+    }
+
+    private static String calculateMinutes(int seconds) {
+        StringBuilder stringBuilder = new StringBuilder();
+        seconds = seconds % 3600;
+        int minutes = seconds / 60;
+        if (isZeroNeeded(minutes)) {
+            stringBuilder.append("0");
+        }
+        stringBuilder
+                .append(minutes);
+        return stringBuilder.toString();
     }
 
     private static boolean isZeroNeeded(int time) {
